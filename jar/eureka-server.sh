0c0eab14e35fde987971795934dd237848ee9f3b
1549145229@qq.com <1549145229@qq.com>
2019-02-07 12:39:23 +0800

    eureka-server.sh

---
+++ jar/eureka-server.sh
@@ -0,0 +1,4 @@
+java -jar eureka-1.0-SNAPSHOT.jar --spring.config.location=classpath:application-zone1a.properties &
+java -jar eureka-1.0-SNAPSHOT.jar --spring.config.location=classpath:application-zone1b.properties &
+java -jar eureka-1.0-SNAPSHOT.jar --spring.config.location=classpath:application-zone2a.properties &
+java -jar eureka-1.0-SNAPSHOT.jar --spring.config.location=classpath:application-zone2b.properties &
