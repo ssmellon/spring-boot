54bd4aba9245e070db6c30ac8868119e4028553c
1549145229@qq.com <1549145229@qq.com>
2019-02-10 14:04:51 +0800

    gateway canary test

--- HelloEurakaClient/src/main/java/com/karl/pre/EurekaClientApplication.java
+++ HelloEurakaClient/src/main/java/com/karl/pre/EurekaClientApplication.java
@@ -1,4 +1,15 @@
 package com.karl.pre;
 
-public class EurekaClientApplication {
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class EurekaClientApplication
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(EurekaClientApplication.class, args);
+    }
 }
--- HelloEurakaClient/src/main/java/com/karl/pre/web/ClientController.java
+++ HelloEurakaClient/src/main/java/com/karl/pre/web/ClientController.java
@@ -1,4 +1,49 @@
 package com.karl.pre.web;
 
-public class ClientController {
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class ClientController
+{
+    @GetMapping("/hello")
+    public ResponseEntity<String> hello()
+    {
+        return new ResponseEntity<>("get hello", HttpStatus.OK);
+    }
+
+    @PostMapping("/hello")
+    public ResponseEntity<String> hello1()
+    {
+        return new ResponseEntity<>("post hello", HttpStatus.OK);
+    }
+
+    @PostMapping("/world/{path}")
+    public ResponseEntity<Map<String, String>> world(@PathVariable("path") String path,
+                                            @RequestParam("wor") String wor,
+                                            @RequestBody String hello)
+    {
+        Map<String, String> ret = new HashMap<>();
+        ret.put("path", path);
+        ret.put("wor", wor);
+        ret.put("hello", hello);
+        return new ResponseEntity<>(ret, HttpStatus.OK);
+    }
+
+    /** test the canary */
+    @GetMapping("/v1")
+    public ResponseEntity<String> v1()
+    {
+        return new ResponseEntity<>("get v1", HttpStatus.OK);
+    }
+
+    @GetMapping("/v2")
+    public ResponseEntity<String> v2()
+    {
+        return new ResponseEntity<>("get v2", HttpStatus.OK);
+    }
 }
