79ff16c6e5e266893735bd6e7e1df2d0c190d346
1549145229@qq.com <1549145229@qq.com>
2019-03-10 11:09:39 +0800

    eureka client consumer

--- HelloEurekaClientConsumer/src/main/java/com/karl/pre/controller/QueryController.java
+++ HelloEurekaClientConsumer/src/main/java/com/karl/pre/controller/QueryController.java
@@ -11,12 +11,14 @@ import org.springframework.cloud.netflix.eureka.serviceregistry.EurekaServiceReg
 import org.springframework.context.ApplicationContext;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 import javax.xml.ws.Response;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by caibosi on 2018-06-20.
@@ -25,19 +27,40 @@ import java.util.List;
 @RequestMapping("")
 public class QueryController
 {
-//    @Autowired
-//    DiscoveryClient discoveryClient;
+    @Autowired
+    DiscoveryClient discoveryClient;
 
     @Autowired
     RestTemplate restTemplate;
 
-    @GetMapping("/consumer/{service}")
-    public String getServices(@PathVariable("service") String service)
+    @PostMapping("/consumer/{service}")
+    public String getServices(@PathVariable("service") String service,
+                              @RequestBody Map<String, String> params)
     {
-        String url = "http://" + service + "/hello";
-        ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.GET, new HttpEntity<>(null), String.class);
-
+        String path = params.getOrDefault("path", "/hello");
+        System.out.println("path  " + path);
+        String url = "http://" + service + path;
+        ResponseEntity<String> response = new ResponseEntity<>("origin ", HttpStatus.OK);
+        try
+        {
+            response = restTemplate.exchange(url, HttpMethod.GET, new HttpEntity<>(null), String.class);
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
         return response.getBody();
     }
 
+    @GetMapping("/service/{serviceId}")
+    public List<ServiceInstance> getInstance(@PathVariable("serviceId") String serviceId)
+    {
+        return discoveryClient.getInstances(serviceId);
+    }
+
+    @GetMapping("/services")
+    public List<String> getServices()
+    {
+        return discoveryClient.getServices();
+    }
 }
